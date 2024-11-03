caddc0e53d5a62297922b100c5a54383fc583a61
yamilobt8 <yamilo12345678910@gmail.com>
2024-11-03 17:43:01 +0100

    14 out of 50 Problems Solved

---
+++ Special_Pythagorean_Triplet.py
@@ -0,0 +1,10 @@
+def find_pythagorean_triplets(sum_total):
+    for a in range(1, sum_total // 3): # reduce time complexity by reducing the loop end (a < b)
+        for b in range(a + 1, sum_total // 2): # b != a and b > a
+            c = sum_total - a - b # a + b + c = 1000 -> c = 1000 - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                return a * b * c # return the product abc
+    return 0
+
+triplet = find_pythagorean_triplets(1000)
+print(triplet) # 31875000
